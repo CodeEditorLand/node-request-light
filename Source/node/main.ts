1d1661a59e1df16397165bb3fe793fb61e42f6b6
Nikola Hristov <Nikola@PlayForm.Cloud>
2024-05-26 14:19:34 +0300

    squash!

--- Source/node/main.ts
+++ Source/node/main.ts
@@ -2,16 +2,23 @@
  *  Copyright (c) Microsoft Corporation. All rights reserved.
  *  Licensed under the MIT License. See License.txt in the project root for license information.
  *--------------------------------------------------------------------------------------------*/
-import * as http from 'http';
-import * as https from 'https';
-import { format, parse as parseUrl, Url } from 'url';
-import * as nls from 'vscode-nls';
-import * as zlib from 'zlib';
+import * as http from "http";
+import * as https from "https";
+import { type Url, format, parse as parseUrl } from "url";
+import * as zlib from "zlib";
+import * as nls from "vscode-nls";
 
-import * as createHttpProxyAgent from 'http-proxy-agent';
-import * as createHttpsProxyAgent from 'https-proxy-agent';
+import * as createHttpProxyAgent from "http-proxy-agent";
+import * as createHttpsProxyAgent from "https-proxy-agent";
 
-import { HttpProxyAgent, HttpsProxyAgent, XHRConfigure, XHROptions, XHRRequest, XHRResponse } from '../../api';
+import type {
+	HttpProxyAgent,
+	HttpsProxyAgent,
+	XHRConfigure,
+	XHROptions,
+	XHRRequest,
+	XHRResponse,
+} from "../../api";
 
 if (process.env.VSCODE_NLS_CONFIG) {
 	const VSCODE_NLS_CONFIG = process.env.VSCODE_NLS_CONFIG;
@@ -20,9 +27,12 @@ if (process.env.VSCODE_NLS_CONFIG) {
 const localize = nls.loadMessageBundle();
 
 let proxyUrl: string | undefined = undefined;
-let strictSSL: boolean = true;
+let strictSSL = true;
 
-export const configure: XHRConfigure = (_proxyUrl: string | undefined, _strictSSL: boolean) => {
+export const configure: XHRConfigure = (
+	_proxyUrl: string | undefined,
+	_strictSSL: boolean,
+) => {
 	proxyUrl = _proxyUrl;
 	strictSSL = _strictSSL;
 };
@@ -30,136 +40,179 @@ export const configure: XHRConfigure = (_proxyUrl: string | undefined, _strictSS
 export const xhr: XHRRequest = (options: XHROptions): Promise<XHRResponse> => {
 	options = { ...options };
 
-	if (typeof options.strictSSL !== 'boolean') {
+	if (typeof options.strictSSL !== "boolean") {
 		options.strictSSL = strictSSL;
 	}
 	if (!options.agent) {
 		options.agent = getProxyAgent(options.url, { proxyUrl, strictSSL });
 	}
-	if (typeof options.followRedirects !== 'number') {
+	if (typeof options.followRedirects !== "number") {
 		options.followRedirects = 5;
 	}
 
-	return request(options).then(result => new Promise<XHRResponse>((c, e) => {
-		const res = result.res;
-		let readable: import('stream').Readable = res;
-		let isCompleted = false;
+	return request(options).then(
+		(result) =>
+			new Promise<XHRResponse>((c, e) => {
+				const res = result.res;
+				let readable: import("stream").Readable = res;
+				let isCompleted = false;
 
-		const encoding = res.headers && res.headers['content-encoding'];
-		if (encoding && !hasNoBody(options.type, result.res.statusCode)) {
-			const zlibOptions = {
-				flush: zlib.constants.Z_SYNC_FLUSH,
-				finishFlush: zlib.constants.Z_SYNC_FLUSH
-			};
-			if (encoding === 'gzip') {
-				const gunzip = zlib.createGunzip(zlibOptions);
-				res.pipe(gunzip);
-				readable = gunzip;
-			} else if (encoding === 'deflate') {
-				const inflate = zlib.createInflate(zlibOptions);
-				res.pipe(inflate);
-				readable = inflate;
-			}
-		}
-		const data: any = [];
-		readable.on('data', c => data.push(c));
-		readable.on('end', () => {
-			if (isCompleted) {
-				return;
-			}
-			isCompleted = true;
-			if (options.followRedirects > 0 && (res.statusCode >= 300 && res.statusCode <= 303 || res.statusCode === 307)) {
-				let location = res.headers['location'];
-				if (location.startsWith('/')) {
-					const endpoint = parseUrl(options.url);
-					location = format({
-						protocol: endpoint.protocol,
-						hostname: endpoint.hostname,
-						port: endpoint.port,
-						pathname: location
+				const encoding = res.headers && res.headers["content-encoding"];
+				if (
+					encoding &&
+					!hasNoBody(options.type, result.res.statusCode)
+				) {
+					const zlibOptions = {
+						flush: zlib.constants.Z_SYNC_FLUSH,
+						finishFlush: zlib.constants.Z_SYNC_FLUSH,
+					};
+					if (encoding === "gzip") {
+						const gunzip = zlib.createGunzip(zlibOptions);
+						res.pipe(gunzip);
+						readable = gunzip;
+					} else if (encoding === "deflate") {
+						const inflate = zlib.createInflate(zlibOptions);
+						res.pipe(inflate);
+						readable = inflate;
+					}
+				}
+				const data: any = [];
+				readable.on("data", (c) => data.push(c));
+				readable.on("end", () => {
+					if (isCompleted) {
+						return;
+					}
+					isCompleted = true;
+					if (
+						options.followRedirects > 0 &&
+						((res.statusCode >= 300 && res.statusCode <= 303) ||
+							res.statusCode === 307)
+					) {
+						let location = res.headers["location"];
+						if (location.startsWith("/")) {
+							const endpoint = parseUrl(options.url);
+							location = format({
+								protocol: endpoint.protocol,
+								hostname: endpoint.hostname,
+								port: endpoint.port,
+								pathname: location,
+							});
+						}
+						if (location) {
+							const newOptions: XHROptions = {
+								type: options.type,
+								url: location,
+								user: options.user,
+								password: options.password,
+								headers: options.headers,
+								timeout: options.timeout,
+								followRedirects: options.followRedirects - 1,
+								data: options.data,
+								token: options.token,
+							};
+							xhr(newOptions).then(c, e);
+							return;
+						}
+					}
+
+					const buffer = Buffer.concat(data);
+
+					const response: XHRResponse = {
+						responseText: buffer.toString(),
+						body: buffer,
+						status: res.statusCode,
+						headers: res.headers || {},
+					};
+
+					if (
+						(res.statusCode >= 200 && res.statusCode < 300) ||
+						res.statusCode === 1223
+					) {
+						c(response);
+					} else {
+						e(response);
+					}
+				});
+				readable.on("error", (err) => {
+					let response: XHRResponse | Error;
+					if (AbortError.is(err)) {
+						response = err;
+					} else {
+						response = {
+							responseText: localize(
+								"error",
+								"Unable to access {0}. Error: {1}",
+								options.url,
+								err.message,
+							),
+							body: Buffer.concat(data),
+							status: 500,
+							headers: {},
+						};
+					}
+					isCompleted = true;
+					e(response);
+				});
+
+				if (options.token) {
+					if (options.token.isCancellationRequested) {
+						readable.destroy(new AbortError());
+					}
+					options.token.onCancellationRequested(() => {
+						readable.destroy(new AbortError());
 					});
 				}
-				if (location) {
-					const newOptions: XHROptions = {
-						type: options.type, url: location, user: options.user, password: options.password, headers: options.headers,
-						timeout: options.timeout, followRedirects: options.followRedirects - 1, data: options.data, token: options.token
-					};
-					xhr(newOptions).then(c, e);
-					return;
-				}
-			}
-
-			const buffer = Buffer.concat(data);
-
-			const response: XHRResponse = {
-				responseText: buffer.toString(),
-				body: buffer,
-				status: res.statusCode,
-				headers: res.headers || {}
-			};
-
-			if ((res.statusCode >= 200 && res.statusCode < 300) || res.statusCode === 1223) {
-				c(response);
-			} else {
-				e(response);
-			}
-		});
-		readable.on('error', (err) => {
+			}),
+		(err) => {
 			let response: XHRResponse | Error;
 			if (AbortError.is(err)) {
 				response = err;
 			} else {
+				let message: string;
+
+				if (options.agent) {
+					message = localize(
+						"error.cannot.connect.proxy",
+						"Unable to connect to {0} through a proxy. Error: {1}",
+						options.url,
+						err.message,
+					);
+				} else {
+					message = localize(
+						"error.cannot.connect",
+						"Unable to connect to {0}. Error: {1}",
+						options.url,
+						err.message,
+					);
+				}
+
 				response = {
-					responseText: localize('error', 'Unable to access {0}. Error: {1}', options.url, err.message),
-					body: Buffer.concat(data),
-					status: 500,
-					headers: {}
+					responseText: message,
+					body: Buffer.concat([]),
+					status: 404,
+					headers: {},
 				};
 			}
-			isCompleted = true;
-			e(response);
-		});
 
-		if (options.token) {
-			if (options.token.isCancellationRequested) {
-				readable.destroy(new AbortError());
-			}
-			options.token.onCancellationRequested(() => {
-				readable.destroy(new AbortError());
-			});
-		}
-	}), err => {
-		let response: XHRResponse | Error;
-		if (AbortError.is(err)) {
-			response = err;
-		} else {
-			let message: string;
-
-			if (options.agent) {
-				message = localize('error.cannot.connect.proxy', 'Unable to connect to {0} through a proxy. Error: {1}', options.url, err.message);
-			} else {
-				message = localize('error.cannot.connect', 'Unable to connect to {0}. Error: {1}', options.url, err.message);
-			}
-
-			response = {
-				responseText: message,
-				body: Buffer.concat([]),
-				status: 404,
-				headers: {}
-			};
-		}
-
-		return Promise.reject(response);
-	});
-}
+			return Promise.reject(response);
+		},
+	);
+};
 
 function assign(destination: any, ...sources: any[]): any {
-	sources.forEach(source => Object.keys(source).forEach((key) => destination[key] = source[key]));
+	sources.forEach((source) =>
+		Object.keys(source).forEach((key) => (destination[key] = source[key])),
+	);
 	return destination;
 }
 
 function hasNoBody(method: string, code: number) {
-	return method === 'HEAD' || /* Informational */ (code >= 100 && code < 200) || /* No Content */  code === 204 || /* Not Modified */ code === 304;
+	return (
+		method === "HEAD" ||
+		/* Informational */ (code >= 100 && code < 200) ||
+		/* No Content */ code === 204 ||
+		/* Not Modified */ code === 304
+	);
 }
 
 interface RequestResult {
@@ -176,43 +229,58 @@ function request(options: XHROptions): Promise<RequestResult> {
 		const opts: https.RequestOptions = {
 			hostname: endpoint.hostname,
 			agent: options.agent ? options.agent : false,
-			port: endpoint.port ? parseInt(endpoint.port) : (endpoint.protocol === 'https:' ? 443 : 80),
+			port: endpoint.port
+				? Number.parseInt(endpoint.port)
+				: endpoint.protocol === "https:"
+					? 443
+					: 80,
 			path: endpoint.path,
-			method: options.type || 'GET',
+			method: options.type || "GET",
 			headers: options.headers,
-			rejectUnauthorized: (typeof options.strictSSL === 'boolean') ? options.strictSSL : true
+			rejectUnauthorized:
+				typeof options.strictSSL === "boolean"
+					? options.strictSSL
+					: true,
 		};
 
 		if (options.user && options.password) {
-			opts.auth = options.user + ':' + options.password;
+			opts.auth = options.user + ":" + options.password;
 		}
 
 		const handler = (res: http.IncomingMessage) => {
-			if (res.statusCode >= 300 && res.statusCode < 400 && options.followRedirects && options.followRedirects > 0 && res.headers['location']) {
-				let location = res.headers['location'];
-				if (location.startsWith('/')) {
+			if (
+				res.statusCode >= 300 &&
+				res.statusCode < 400 &&
+				options.followRedirects &&
+				options.followRedirects > 0 &&
+				res.headers["location"]
+			) {
+				let location = res.headers["location"];
+				if (location.startsWith("/")) {
 					location = format({
 						protocol: endpoint.protocol,
 						hostname: endpoint.hostname,
 						port: endpoint.port,
-						pathname: location
+						pathname: location,
 					});
 				}
-				c(<any>request(assign({}, options, {
-					url: location,
-					followRedirects: options.followRedirects - 1
-				})));
+				c(<any>request(
+						assign({}, options, {
+							url: location,
+							followRedirects: options.followRedirects - 1,
+						}),
+					));
 			} else {
 				c({ req, res });
 			}
-		}
-		if (endpoint.protocol === 'https:') {
+		};
+		if (endpoint.protocol === "https:") {
 			req = https.request(opts, handler);
 		} else {
 			req = http.request(opts, handler);
 		}
 
-		req.on('error', e);
+		req.on("error", e);
 
 		if (options.timeout) {
 			req.setTimeout(options.timeout);
@@ -239,36 +307,116 @@ export function getErrorStatusDescription(status: number): string {
 		return void 0;
 	}
 	switch (status) {
-		case 400: return localize('status.400', 'Bad request. The request cannot be fulfilled due to bad syntax.');
-		case 401: return localize('status.401', 'Unauthorized. The server is refusing to respond.');
-		case 403: return localize('status.403', 'Forbidden. The server is refusing to respond.');
-		case 404: return localize('status.404', 'Not Found. The requested location could not be found.');
-		case 405: return localize('status.405', 'Method not allowed. A request was made using a request method not supported by that location.');
-		case 406: return localize('status.406', 'Not Acceptable. The server can only generate a response that is not accepted by the client.');
-		case 407: return localize('status.407', 'Proxy Authentication Required. The client must first authenticate itself with the proxy.');
-		case 408: return localize('status.408', 'Request Timeout. The server timed out waiting for the request.');
-		case 409: return localize('status.409', 'Conflict. The request could not be completed because of a conflict in the request.');
-		case 410: return localize('status.410', 'Gone. The requested page is no longer available.');
-		case 411: return localize('status.411', 'Length Required. The "Content-Length" is not defined.');
-		case 412: return localize('status.412', 'Precondition Failed. The precondition given in the request evaluated to false by the server.');
-		case 413: return localize('status.413', 'Request Entity Too Large. The server will not accept the request, because the request entity is too large.');
-		case 414: return localize('status.414', 'Request-URI Too Long. The server will not accept the request, because the URL is too long.');
-		case 415: return localize('status.415', 'Unsupported Media Type. The server will not accept the request, because the media type is not supported.');
-		case 500: return localize('status.500', 'Internal Server Error.');
-		case 501: return localize('status.501', 'Not Implemented. The server either does not recognize the request method, or it lacks the ability to fulfill the request.');
-		case 502: return localize('status.502', 'Bad Gateway. The upstream server did not respond.');
-		case 503: return localize('status.503', 'Service Unavailable. The server is currently unavailable (overloaded or down).');
-		default: return localize('status.416', 'HTTP status code {0}', status);
+		case 400:
+			return localize(
+				"status.400",
+				"Bad request. The request cannot be fulfilled due to bad syntax.",
+			);
+		case 401:
+			return localize(
+				"status.401",
+				"Unauthorized. The server is refusing to respond.",
+			);
+		case 403:
+			return localize(
+				"status.403",
+				"Forbidden. The server is refusing to respond.",
+			);
+		case 404:
+			return localize(
+				"status.404",
+				"Not Found. The requested location could not be found.",
+			);
+		case 405:
+			return localize(
+				"status.405",
+				"Method not allowed. A request was made using a request method not supported by that location.",
+			);
+		case 406:
+			return localize(
+				"status.406",
+				"Not Acceptable. The server can only generate a response that is not accepted by the client.",
+			);
+		case 407:
+			return localize(
+				"status.407",
+				"Proxy Authentication Required. The client must first authenticate itself with the proxy.",
+			);
+		case 408:
+			return localize(
+				"status.408",
+				"Request Timeout. The server timed out waiting for the request.",
+			);
+		case 409:
+			return localize(
+				"status.409",
+				"Conflict. The request could not be completed because of a conflict in the request.",
+			);
+		case 410:
+			return localize(
+				"status.410",
+				"Gone. The requested page is no longer available.",
+			);
+		case 411:
+			return localize(
+				"status.411",
+				'Length Required. The "Content-Length" is not defined.',
+			);
+		case 412:
+			return localize(
+				"status.412",
+				"Precondition Failed. The precondition given in the request evaluated to false by the server.",
+			);
+		case 413:
+			return localize(
+				"status.413",
+				"Request Entity Too Large. The server will not accept the request, because the request entity is too large.",
+			);
+		case 414:
+			return localize(
+				"status.414",
+				"Request-URI Too Long. The server will not accept the request, because the URL is too long.",
+			);
+		case 415:
+			return localize(
+				"status.415",
+				"Unsupported Media Type. The server will not accept the request, because the media type is not supported.",
+			);
+		case 500:
+			return localize("status.500", "Internal Server Error.");
+		case 501:
+			return localize(
+				"status.501",
+				"Not Implemented. The server either does not recognize the request method, or it lacks the ability to fulfill the request.",
+			);
+		case 502:
+			return localize(
+				"status.502",
+				"Bad Gateway. The upstream server did not respond.",
+			);
+		case 503:
+			return localize(
+				"status.503",
+				"Service Unavailable. The server is currently unavailable (overloaded or down).",
+			);
+		default:
+			return localize("status.416", "HTTP status code {0}", status);
 	}
 }
 
 // proxy handling
 
 function getSystemProxyURI(requestURL: Url): string {
-	if (requestURL.protocol === 'http:') {
+	if (requestURL.protocol === "http:") {
 		return process.env.HTTP_PROXY || process.env.http_proxy || null;
-	} else if (requestURL.protocol === 'https:') {
-		return process.env.HTTPS_PROXY || process.env.https_proxy || process.env.HTTP_PROXY || process.env.http_proxy || null;
+	} else if (requestURL.protocol === "https:") {
+		return (
+			process.env.HTTPS_PROXY ||
+			process.env.https_proxy ||
+			process.env.HTTP_PROXY ||
+			process.env.http_proxy ||
+			null
+		);
 	}
 
 	return null;
@@ -279,7 +427,10 @@ interface ProxyOptions {
 	strictSSL?: boolean;
 }
 
-function getProxyAgent(rawRequestURL: string, options: ProxyOptions = {}): HttpProxyAgent | HttpsProxyAgent | undefined {
+function getProxyAgent(
+	rawRequestURL: string,
+	options: ProxyOptions = {},
+): HttpProxyAgent | HttpsProxyAgent | undefined {
 	const requestURL = parseUrl(rawRequestURL);
 	const proxyURL = options.proxyUrl || getSystemProxyURI(requestURL);
 
@@ -297,17 +448,20 @@ function getProxyAgent(rawRequestURL: string, options: ProxyOptions = {}): HttpP
 		host: proxyEndpoint.hostname,
 		port: Number(proxyEndpoint.port),
 		auth: proxyEndpoint.auth,
-		rejectUnauthorized: (typeof options.strictSSL === 'boolean') ? options.strictSSL : true,
-		protocol: proxyEndpoint.protocol
+		rejectUnauthorized:
+			typeof options.strictSSL === "boolean" ? options.strictSSL : true,
+		protocol: proxyEndpoint.protocol,
 	};
 
-	return requestURL.protocol === 'http:' ? createHttpProxyAgent(opts) : createHttpsProxyAgent(opts);
+	return requestURL.protocol === "http:"
+		? createHttpProxyAgent(opts)
+		: createHttpsProxyAgent(opts);
 }
 
 class AbortError extends Error {
 	constructor() {
-		super('The user aborted a request');
-		this.name = 'AbortError';
+		super("The user aborted a request");
+		this.name = "AbortError";
 
 		// see https://github.com/microsoft/TypeScript/issues/13965
 		Object.setPrototypeOf(this, AbortError.prototype);
